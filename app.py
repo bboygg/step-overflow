3ad611d50139a285a421f494ab610cc6314be444
bboygg <bboygg94@gmail.com>
2022-07-13 21:59:51 +0900

    add file structure

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST:
+
+        return redirect("/")
+
+    else:
+
+        return render_template("index.html")
+
+
